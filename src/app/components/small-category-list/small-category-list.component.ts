dac01706499fc7175708cc0ea4424a26ca83a78c
sandrors <sandro.suknidze.1@btu.edu.ge>
2024-10-18 16:21:51 +0400

    bug fixes

--- src/app/components/small-category-list/small-category-list.component.ts
+++ src/app/components/small-category-list/small-category-list.component.ts
@@ -1,35 +1,53 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-small-category-list',
-  templateUrl: './small-category-list.component.html',
-  styleUrl: './small-category-list.component.css'
+    selector: 'app-small-category-list',
+    templateUrl: './small-category-list.component.html',
+    styleUrl: './small-category-list.component.css',
 })
 export class SmallCategoryListComponent implements AfterViewInit {
-  showMore = false;
-  isOverflowing = false;
+    showMore = false;
+    isOverflowing = false;
 
-  names = [
-    'Name 1', 'Name 2', 'Name 3', 'Name 4', 'Name 5', 
-    'Name 6', 'Name 7', 'Name 8', 'Name 9', 'Name 10',
-    'Name 11', 'Name 12', 'Name 13', 'Name 14', 'Name 15'
-  ];
+    names = [
+        'Name 1',
+        'Name 2',
+        'Name 3',
+        'Name 4',
+        'Name 5',
+        'Name 6',
+        'Name 7',
+        'Name 8',
+        'Name 9',
+        'Name 10',
+        'Name 11',
+        'Name 12',
+        'Name 13',
+        'Name 14',
+        'Name 15',
+        'Name 16',
+        'Name 17',
+        'Name 18',
+        'Name 19',
+        'Name 20',
+    ];
 
-  @ViewChild('nameListContainer') nameListContainer!: ElementRef;
+    @ViewChild('nameListContainer') nameListContainer!: ElementRef;
 
-  ngAfterViewInit() {
-    this.checkOverflow();
-  }
-
-  toggleShowMore() {
-    this.showMore = !this.showMore;
-  }
-
-  checkOverflow() {
-    const container = this.nameListContainer.nativeElement;
-    console.log(container);
-    if (container.scrollHeight > container.clientHeight) {
-      this.isOverflowing = true;
+    ngAfterViewInit() {
+        setTimeout(() => {
+            this.checkOverflow();
+        });
     }
-  }
+
+    toggleShowMore() {
+        this.showMore = !this.showMore;
+    }
+
+    checkOverflow() {
+        const container = this.nameListContainer.nativeElement;
+        if (container.scrollHeight > container.clientHeight) {
+            this.isOverflowing = true;
+        }
+    }
 }
