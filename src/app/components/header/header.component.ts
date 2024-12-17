05dc224246ec02dcf3b741d2843aaa60c9bdd028
Sandro Ubuntu <sandro.suknidze.1@btu.edu.ge>
2024-12-17 23:40:32 +0400

    + Search ui

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,70 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormBuilder} from "@angular/forms";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  isModalOpen: boolean = false;
+    private formBuilder = inject(FormBuilder);
 
-  toggleModal() {
-    this.isModalOpen = !this.isModalOpen;
-  }
+    isModalOpen: boolean = false;
+
+    searchForm = this.formBuilder.group({
+        name: [''],
+        category: [''],
+    });
+
+    items: any[] = [
+        { id: 1, name: 'Apple', category: 'Fruit' },
+        { id: 2, name: 'Carrot', category: 'Vegetable' },
+        { id: 3, name: 'Banana', category: 'Fruit' },
+        { id: 4, name: 'Tomato', category: 'Vegetable' },
+        { id: 5, name: 'Grapes', category: 'Fruit' },
+        { id: 6, name: 'Potato', category: 'Vegetable' },
+    ];
+
+    filteredItems: any[] = [];
+
+    constructor() {
+        // Initially show all items
+        this.filteredItems = this.items;
+
+        // Listen to changes in form and filter items dynamically
+        this.searchForm.valueChanges.subscribe(() => this.filterItems());
+    }
+
+    filterItems(): void {
+        // @ts-ignore
+        const { name, category } = this.searchForm.value;
+
+        // Apply filters
+        this.filteredItems = this.items.filter((item) => {
+            const matchesName = name
+                ? item.name.toLowerCase().includes(name.toLowerCase())
+                : true;
+
+            const matchesCategory = category
+                ? item.category.toLowerCase().includes(category.toLowerCase())
+                : true;
+
+            // Combine both conditions
+            return matchesName && matchesCategory;
+        });
+    }
+
+
+
+
+    toggleModal() {
+        this.isModalOpen = !this.isModalOpen;
+    }
+
+
+
+
+
+
+
 }
